016dbd4d81cf4a3a60cda66be3abaa923cac29ed
donjoe <giumontanaro@gmail.com>
2018-04-02 21:01:01 +0200

    update the position of enemies as well as their speed

--- js/app.js
+++ js/app.js
@@ -6,17 +6,31 @@ ENEMY CONSTRUCTOR/PROTOTYPE SECTION
 var Enemy = function(x, y, speed) {
   this.x = x;
   this.y = y;
+  this.speed = speed;
   this.sprite = 'images/enemy-bug.png';
 };
 
 
-Enemy.prototype.update = function(dt) {
-
-};
-
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function moveBugs(dt) {
+
+    this.x += this.speed * dt * 100;
+    //if the position of bugs cross the grid edges, set their x-position back to beginning and increase their speed
+    if( (this.x - 101) > 505) {
+      this.x = - 100;
+      this.speed++;
+
+      if(this.speed > 6) {
+        this.speed = 1;
+      }//avoid extreme speeds
+    }
+};
+
 
 /*
 -----------------------------------
@@ -77,7 +91,7 @@ Player.prototype.handleInput = function(input) {
 INSTANTIATE ENEMIES AND PLAYER
 ---------------------------*/
 const allEnemies = [];
-allEnemies.push(new Enemy(-100, 67), new Enemy(-90, 233), new Enemy(-200, 150));
+allEnemies.push(new Enemy(-100, 67, 1), new Enemy(-90, 233, 2), new Enemy(-200, 67, 3 ), new Enemy(-200, 150, 2.5 ), new Enemy(-200, 150, 1.5 ));
 
 const player = new Player(202, 399);
 
