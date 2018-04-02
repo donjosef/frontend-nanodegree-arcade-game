7c556acfe4140c4a271b744123cb7d429c851a19
donjoe <giumontanaro@gmail.com>
2018-04-02 11:31:47 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+var Enemy = function() {
+  
+    this.sprite = 'images/enemy-bug.png';
+};
+
+
+Enemy.prototype.update = function(dt) {
+    
+};
+
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
